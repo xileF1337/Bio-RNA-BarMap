07fd71a6d4ec263bc60164950b8efe2ca22094de
Felix Kuehnl <felix@bioinf.uni-leipzig.de>
2021-11-12 17:11:41 +0100

    Updated documentation

--- lib/Bio/RNA/BarMap/Mapping/Type.pm
+++ lib/Bio/RNA/BarMap/Mapping/Type.pm
@@ -65,6 +65,144 @@ sub arrow {
 
 __PACKAGE__->meta->make_immutable;
 
-1;
+1;              # End of Bio::RNA::BarMap::Mapping::Type
+
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Bio::RNA::BarMap::Mapping::Type - Represents the type of a I<BarMap> mapping
+(exact or approximate)
+
+=head1 SYNOPSIS
+
+    use v5.12;                                              # for 'say'
+    use Bio::RNA::BarMap;
+
+    # Get a new mapping type object ... from arrow string:
+    my $type = Bio::RNA::BarMap::Mapping::Type->new('->');  # exact
+       $type = Bio::RNA::BarMap::Mapping::Type->new('~>');  # approx
+
+    # ... or programmatically:
+    $type = Bio::RNA::BarMap::Mapping::Type->exact;         # exact
+    $type = Bio::RNA::BarMap::Mapping::Type->approx;        # approx
+
+    # Verify mapping type.
+    say 'Mapping is ', $type->is_exact  ? 'exact' : 'approximate';
+    say 'Mapping arrow: ', $type->arrow;
+
+
+=head1 DESCRIPTION
+
+The objects of this class are used to represent the two possible types of a
+mapping, either exact or approximate, and to easily convert between these two
+values and their respective arrow representation. In I<BarMap> files,
+C<< -> >> denotes an exact mapping, and C<< ~> >> an approximate one. The
+object is constructed from the arrow string via the constructor C<new()>, or
+using the methods C<exact()> and C<approx()> to get a new object of the
+respective type.
+
+
+=head1 METHODS
+
+=head2 Bio::RNA::BarMap::Mapping::Type->new($arrow_string)
+
+Constructs a new type object from its arrow string representation, i. e.
+C<< -> >> for the exact and C<< ~> >> for the approximate variant.
+
+=head2 Bio::RNA::BarMap::Mapping::Type->exact()
+
+Class method. Returns a new mapping type object of I<exact> type.
+
+=head2 Bio::RNA::BarMap::Mapping::Type->approx()
+
+Class method. Returns a new mapping type object of I<approximate> type.
+
+=head2 $type->is_exact()
+
+Returns true iff the current object is of I<exact> type.
+
+=head2 $type->is_approx()
+
+Returns true iff the current object is of I<approximate> type.
+
+=head2 $type->arrow()
+
+Returns the type of this object in its arrow string representation, i. e.
+C<< -> >> for the exact and C<< ~> >> for the approximate.
+
+=head1 AUTHOR
+
+Felix Kuehnl, C<< <felix at bioinf.uni-leipzig.de> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-bio-rna-barmap at
+rt.cpan.org>, or through the web interface at
+L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Bio-RNA-BarMap>.  I will be
+notified, and then you'll automatically be notified of progress on your bug as
+I make changes.
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Bio::RNA::BarMap
+
+
+You can also look for information at the official BarMap website:
+
+L<https://www.tbi.univie.ac.at/RNA/bar_map/>
+
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Bio-RNA-BarMap>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Bio-RNA-BarMap>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/Bio-RNA-BarMap>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/Bio-RNA-BarMap>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2019 Felix Kuehnl.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see L<http://www.gnu.org/licenses/>.
+
+
+=cut
 
 # End of Bio::RNA::BarMap::Mapping::Type / lib/Bio/RNA/BarMap/Mapping/Type.pm
