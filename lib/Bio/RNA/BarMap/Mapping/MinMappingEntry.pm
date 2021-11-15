036a0cac4bb546e7615da4df9e1fdbd226298cc3
Felix Kuehnl <felix@bioinf.uni-leipzig.de>
2021-11-15 15:45:29 +0100

    Removed Set::Scalar dependency

--- lib/Bio/RNA/BarMap/Mapping/MinMappingEntry.pm
+++ lib/Bio/RNA/BarMap/Mapping/MinMappingEntry.pm
@@ -9,7 +9,6 @@ use Moose;
 use namespace::autoclean;
 
 use Scalar::Util qw( weaken );
-use Set::Scalar;
 
 use Bio::RNA::BarMap::Mapping::Type;
 
@@ -21,16 +20,23 @@ has 'to_type' => (
 );
 
 # Ensure object is cleaned after use => use weak refs
-# Init 'from' with empty array ref to immedtialey push to it.
-# has '_from' => (is => 'ro', default => sub { Set::Scalar->new() });
-has '_from' => (is => 'ro', default => sub { Bio::RNA::BarMap::Mapping::Set->new });
-has 'to'    => (is => 'rw', weak_ref => 1, predicate => 'has_to');
+has '_from' => (
+    is      => 'ro',
+    default => sub { Bio::RNA::BarMap::Mapping::Set->new },
+);
+
+has 'to' => (
+    is          => 'rw',
+    weak_ref    => 1,
+    predicate   => 'has_to',
+    isa         => __PACKAGE__,             # another entry
+);
 
 # Always use this method to add 'from' minima. This ensures the refs
 # are weakened and no memory leaks arise.
 sub add_from {
     my ($self, @from) = @_;
-    weaken $_ foreach @from;        # turn into weak references
+    weaken $_ foreach @from;                # turn into weak references
     $self->_from->insert(@from);
 }
 
@@ -42,6 +48,151 @@ sub get_from {
 
 __PACKAGE__->meta->make_immutable;
 
-1;
+1;          # End of Bio::RNA::BarMap::Mapping::MinMappingEntry
 
-# End of Bio::RNA::BarMap::Mapping::MinMappingEntry
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Bio::RNA::BarMap::Mapping::MinMappingEntry - Store I<BarMap> mappings of a
+single minimum.
+
+=head1 SYNOPSIS
+
+    use v5.12;              # for 'say()' and '//' a.k.a. logical defined-or
+    use Bio::RNA::BarMap;
+
+    my $entry = Bio::RNA::BarMap::Mapping::MinMappingEntry->new(
+        index   => 3,               # of minimum of this entry
+        to      => $to_min,         # the mininimum this one is mapped to
+    );
+
+    # Query the entry.
+    if ($entry->has_to) {           # maps to something
+        say 'This minimum maps ',
+            $entry->$to_type->is_exact ? 'exactly' : 'approximately',
+            ' to minimum ', $entry->to->index;
+    }
+
+    $entry->add_from($from_min_1, $from_min_2);     # add mins mapping to self
+    say "Minima mapped to this minimum:",
+    join q{, }, map {$_->index $entry->get_from();
+
+
+=head1 DESCRIPTION
+
+Internal class used to store the mapping of a single minimum. Both the forward
+direction ("target minimum", C<to()>) and the reverse direction ("source
+minima", C<get_from()>) are provided. While the target minimum is unique, but
+not necessarily defined (cf. C<has_to()>), there may be zero to many source
+minima, and so these are stored in a set internally. Use the provided
+methods to add to this set to make sure that the stored references are
+properly weakened and no memory leaks arise.
+
+=head1 METHODS
+
+=head2 Bio::RNA::BarMap::Mapping::MinMappingEntry->new(arg_name => $arg_val, ...)
+
+Constructor of the mapping entry class.
+
+=over
+
+=item Supported arguments:
+
+=over
+
+=item index
+
+Required. Index of the minimum described by this entry.
+
+=item to
+
+=back
+
+=back
+
+=head2 $entry->index
+
+=head2 $entry->to_type
+
+=head2 $entry->to
+
+=head2 $entry->add_from(@from_entries)
+
+=head2 $entry->get_from
+
+
+
+=head1 AUTHOR
+
+Felix Kuehnl, C<< <felix at bioinf.uni-leipzig.de> >>
+
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-bio-rna-barmap at
+rt.cpan.org>, or through the web interface at
+L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Bio-RNA-BarMap>.  I will be
+notified, and then you'll automatically be notified of progress on your bug as
+I make changes.
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Bio::RNA::BarMap
+
+
+You can also look for information at the official BarMap website:
+
+L<https://www.tbi.univie.ac.at/RNA/bar_map/>
+
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Bio-RNA-BarMap>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Bio-RNA-BarMap>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/Bio-RNA-BarMap>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/Bio-RNA-BarMap>
+
+=back
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2019 Felix Kuehnl.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see L<http://www.gnu.org/licenses/>.
+
+
+=cut
+
+# End of Bio/RNA/BarMap/Mapping/MinMappingEntry.pm
